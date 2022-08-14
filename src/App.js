0b5794e2c3963da6993adc6a9713d4bffa3f60ce
Tyler Lampel <lampel13@gmail.com>
2022-08-13 22:34:36 -0400

    start fetch, and db.json file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
+import './CharacterCard';
 
 function App() {
+
+  useEffect(()=> {
+    fetch("https://rickandmortyapi.com/api/character")
+    .then((resp) => resp.json())
+    .then((data) => (console.log(data)))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CharacterCard/>
       </header>
     </div>
   );
