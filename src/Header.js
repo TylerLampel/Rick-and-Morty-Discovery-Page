f9144049bb7695de9c37334171e6b7a3b8ca0e96
Tyler Lampel <lampel13@gmail.com>
2022-08-15 20:52:54 -0400

    Add title

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ function Header(){
     return (
         <header>
           <img src= "./rick-morty-collection-banner_1400x.webp" />
+          <h1>Rick and Morty Discovery Page</h1>
         </header>
     )
 }
