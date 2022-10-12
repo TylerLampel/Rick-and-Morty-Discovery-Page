376bad4ef4d4170899d48999709935bd9b241122
Tyler Lampel <lampel13@gmail.com>
2022-10-12 18:59:25 -0400

    Fixed controlled forms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import CharacterContainer from "./CharacterContainer";
 import NavBar from "./NavBar";
 import EpisodesContainer from "./EpisodesContainer";
@@ -12,89 +12,98 @@ function App() {
   const [locations, setLocations] = useState([]);
   const [episodes, setEpisodes] = useState([]);
 
+  // useEffect, useState, referencing a variable, destructuring
 
-  useEffect(()=> {
-      fetch("http://localhost:3000/characters")
-        .then((resp)=> resp.json())
-        .then((characters) => setCharacters(characters))
-      },[characters])
-      
+  useEffect(() => {
+    fetch("http://localhost:3000/characters")
+      .then((resp) => resp.json())
+      .then((characters) => {
+        setCharacters(characters);
+        console.log("string");
+      });
+  }, []);
+  console.log(characters);
 
-      useEffect(()=> {
-      fetch("http://localhost:3000/locations")
-          .then(resp => resp.json())
-          .then(locations => setLocations(locations))
-      },[locations])
+  useEffect(() => {
+    fetch("http://localhost:3000/locations")
+      .then((resp) => resp.json())
+      .then((locations) => setLocations(locations));
+  }, []);
 
-      useEffect(()=> {
-        fetch("http://localhost:3000/episodes")
-            .then(resp => resp.json())
-            .then(episodes => setEpisodes(episodes))
-        },[])
+  useEffect(() => {
+    fetch("http://localhost:3000/episodes")
+      .then((resp) => resp.json())
+      .then((episodes) => setEpisodes(episodes));
+  }, []);
 
-      function handleAddNewCharacter(character){
-        setCharacters([{character}, ...characters])
+  function handleAddNewCharacter(character) {
+    console.log(character, "character");
+    setCharacters([...characters, character]);
+  }
+
+  function handleRemoveCharacter(id) {
+    const updatedCharacters = characters.filter(
+      (character) => character.id !== id
+    );
+    setCharacters(updatedCharacters);
+  }
+
+  function handleDeleteCharacter(id) {
+    fetch(`http://localhost:3000/characters/${id}`, {
+      method: "DELETE",
+    }).then((resp) => {
+      if (resp.status === 200) {
+        handleRemoveCharacter(id);
       }
+    });
+  }
 
-      function handleRemoveCharacter(id) {
-        const updatedCharacters = characters.filter(character => character.id !== id)
-        setCharacters(updatedCharacters)
-      }
+  function handleAddNewLocation(location) {
+    setLocations([...locations, location]);
+  }
 
-      function handleDeleteCharacter(id) {
-        fetch(`http://localhost:3000/characters/${id}`, {
-          method: "DELETE",
-      }).then((resp)=> {
-        if(resp.status === 200) {
-          handleRemoveCharacter(id)
-        }
-      })
-      }
+  function handleRemoveLocation(id) {
+    const updatedLocations = locations.filter((location) => location.id !== id);
+    setLocations(updatedLocations);
+  }
 
-      function handleAddNewLocation(location){
-        setLocations([{location}, ...locations])
+  function handleDeleteLocation(id) {
+    fetch(`http://localhost:3000/locations/${id}`, {
+      method: "DELETE",
+    }).then((resp) => {
+      if (resp.status === 200) {
+        handleRemoveLocation(id);
       }
-
-      function handleRemoveLocation(id) {
-        const updatedLocations = locations.filter(location => location.id !== id)
-        setLocations(updatedLocations)
-      }
-
-      function handleDeleteLocation(id) {
-        fetch(`http://localhost:3000/locations/${id}`, {
-          method: "DELETE",
-      }).then((resp)=> {
-        if(resp.status === 200) {
-          handleRemoveLocation(id)
-        }
-      })
-      }
+    });
+  }
 
   return (
     <div className="App">
-        <Header />
-        <NavBar />
-        <Route exact path="/">
-          <CharacterContainer 
-            characters={characters}
-            handleAddNewCharacter={handleAddNewCharacter}  
-            handleDeleteCharacter={handleDeleteCharacter}
-          />
-        </Route>
-        <Route exact path="/Episodes">
-          <EpisodesContainer 
-            episodes={episodes} 
+      <Header />
+      <NavBar />
+
+      <Route exact path="/">
+        <CharacterContainer
+          characters={characters}
+          handleAddNewCharacter={handleAddNewCharacter}
+          handleDeleteCharacter={handleDeleteCharacter}
         />
-        </Route>
-        <Route exact path="/Locations">
-          <LocationsContainer 
-            locations={locations}
-            handleAddNewLocation={handleAddNewLocation} 
-            handleDeleteLocation={handleDeleteLocation}
-          />
-        </Route>        
+      </Route>
+      <Route exact path="/Episodes">
+        <EpisodesContainer episodes={episodes} />
+      </Route>
+      <Route exact path="/Locations">
+        <LocationsContainer
+          locations={locations}
+          handleAddNewLocation={handleAddNewLocation}
+          handleDeleteLocation={handleDeleteLocation}
+        />
+      </Route>
     </div>
   );
 }
 
 export default App;
+
+// json-server --watch db.json --port 3000
+// npm start
--- src/CharacterContainer.js
+++ src/CharacterContainer.js
@@ -3,37 +3,45 @@ import CharacterCard from "./CharacterCard";
 import NewCharacterForm from "./NewCharacterForm";
 import { Grid } from "@mui/material";
 
+// two buttons, male and female. when clicked show  only that gender
 
+// add two buttons
+// filter characters array in app pass down filteredCharacters
+// filter characters to only show male/female
 
-function CharacterContainer({ characters, handleAddNewCharacter, handleDeleteCharacter }) {
-    
+function CharacterContainer({
+  characters,
+  handleAddNewCharacter,
+  handleDeleteCharacter,
+}) {
+  const characterCards = characters.map((character) => (
+    <CharacterCard
+      key={character.id}
+      character={character}
+      handleDeleteCharacter={handleDeleteCharacter}
+    />
+  ));
 
-    const characterCards = characters.map((character) => (
-            <CharacterCard
-                key={character["id"]}
-                character={character}
-                handleDeleteCharacter={handleDeleteCharacter}
-            />
-    ))
-    
+  function handleClick() {}
 
-    return (
-        <div id="character-collection" >
-            <NewCharacterForm handleAddNewCharacter={handleAddNewCharacter}/>
-            <br />
-            <br />
-            <Grid 
-                container
-                alignItems="center"
-                justifyContent="center"
-                rowSpacing={3} 
-                columnSpacing={{xs: 1 }}
-            >
-                {characterCards}
-            </ Grid>
-        </div>
-    )
-    
+  return (
+    <div id="character-collection">
+      <NewCharacterForm handleAddNewCharacter={handleAddNewCharacter} />
+      <br />
+      <button onClick={handleClick}>Male</button>
+      <button onClick={handleClick}>Female</button>
+      <br />
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        rowSpacing={3}
+        columnSpacing={{ xs: 1 }}
+      >
+        {characterCards}
+      </Grid>
+    </div>
+  );
 }
 
 export default CharacterContainer;
